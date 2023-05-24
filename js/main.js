560fdb226d7de64ff45cdfbe5c5200148f362c16
fbun <federico.bun@gmail.com>
2023-05-24 13:43:38 -0500

    validación de cantidades de tickets

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 
-
+/* ----- Precio del Ticket ------*/
 function valorTicket () {
     var precioEntrada = 200;
-    var tipo=document.getElementById("cat").value;
-
-    switch (tipo) {
-        case "Estudiante":
-            precioEntrada=precioEntrada*0.2;
-        break;
-        case "Trainee":
-            precioEntrada=precioEntrada*0.5;
-        break;
-        case "Junior":
-            precioEntrada=precioEntrada*0.85;
-        break;
-    }
+    if (document.getElementById('cantidad').value > 0 ) {
+                        var tipo=document.getElementById("cat").value;
+        
+                switch (tipo) {
+                    case "Estudiante":
+                        precioEntrada=precioEntrada*0.2;
+                    break;
+                    case "Trainee":
+                        precioEntrada=precioEntrada*0.5;
+                    break;
+                    case "Junior":
+                        precioEntrada=precioEntrada*0.85;
+                    break;
+                }
+                
+                var precioFinal = document.getElementById('cantidad').value * precioEntrada;
+                document.getElementById("totalApagar").value= "Total a Pagar: $ " +  precioFinal;
+            }
+                else {
+                alert ("La cantidad de Tickets " + document.getElementById('cantidad').value +" solicitado es incorrecta") ;}
+                
     
-    var precioFinal = document.getElementById('cantidad').value * precioEntrada;
-    document.getElementById("totalApagar").value= "Total a Pagar: $ " +  precioFinal;
-
 }
-
 
 document.getElementById('btnResumen').addEventListener('click',valorTicket);
 
